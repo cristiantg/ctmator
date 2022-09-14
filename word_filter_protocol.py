fb602ceb1ee7add751095f6529312a5bcc6d8c54
Cristian TG <cristian.tejedor.91@gmail.com>
2022-09-14 13:38:16 +0200

    hmhm and hmhmhm replaced by uhm (CGN2)

--- word_filter_protocol.py
+++ word_filter_protocol.py
@@ -3,7 +3,7 @@
 
 '''
 This script deals with the following points of the HoMed protocol v2.
-1. Hesitations such as [uh], [hmhm] and [hmhmhm]: keep them.
+1. Hesitations such as [uh]->[uh], [hmhm] and [hmhmhm]->[uhm] (as in CGN2)
 2. All word*char : keeps the left part
 -> word*a --> discard *a an get the left part .
 -> word*d --> word before is a dilect word ---> keep another table for the phonetic transciption.
@@ -14,11 +14,15 @@ This script deals with the following points of the HoMed protocol v2.
 '''
 
 xxx='xxx'
+uhm='uhm'
 
 def v2(word):
-    if xxx in word.lower():
+    w_lower = word.lower()
+    if xxx in w_lower:
         word = xxx
     else:
+        if 'hmhm' in w_lower or 'hmhmhm' in w_lower:
+            word = word.replace('hmhm', uhm).replace('hmhmhm', uhm)
         if '*' in word:
             word = word[:word.index('*')]
         if '[' in word and len(word[:word.index('[')])>0:
